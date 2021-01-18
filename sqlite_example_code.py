2657744e46f0ab5f96afa3b559d94425edc25707
sethdow <seth.dow@gmail.com>
2021-01-18 22:09:22 +0100

    added some more example sqlite commands

--- sqlite_example_code.py
+++ sqlite_example_code.py
@@ -4,6 +4,9 @@ connection = sqlite3.connect('data.db')
 
 cursor = connection.cursor()
 
+drop_table = "DROP TABLE items"
+cursor.execute(drop_table)
+
 create_table = " CREATE TABLE IF NOT EXISTS users (id int, username string, password string)"
 
 cursor.execute(create_table)

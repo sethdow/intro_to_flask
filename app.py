96204449fab56a628c1df5e4249b292834b36a4a
sethdow <seth.dow@gmail.com>
2021-02-06 14:15:28 +0100

    moved import out of if main statement into app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, Item_list
 from resources.store import Store, StoreList 
+from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
